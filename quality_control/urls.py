e296401c5aaf575c4502862d6ae29fccf81fa3a8
Exfurch <alexander.tyurnev@gmail.com>
2024-04-08 16:20:30 +0300

    Task 4 Url View Functions and Classes

--- quality_control/urls.py
+++ quality_control/urls.py
@@ -1,12 +1,17 @@
 from django.urls import path
 from . import views
+from quality_control import views
 
 app_name = 'quality_control'
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    #path('', views.index, name='index'),
     path('bugs/', views.bug_list, name='bug_list'),
     path('features/', views.feature_list, name='feature_list'),
-    path('bugs/<int:bug_id>/', views.bug_detail, name='bug_detail'),  # Маршрут для получения деталей конкретного бага
-    path('features/<int:feature_id>/', views.feature_detail, name='feature_detail'),  # Маршрут для получения деталей конкретного улучшения
+    #path('bugs/<int:bug_id>/', views.bug_detail, name='bug_detail'),  # Маршрут для получения деталей конкретного бага
+    #path('features/<int:feature_id>/', views.feature_detail, name='feature_detail'),  # Маршрут для получения деталей конкретного улучшения
+
+    path('', views.IndexView.as_view(), name='index'),
+    path('bugs/<int:bug_id>/', views.BugReportDetailView.as_view(), name='bug_detail'),
+    path('features/<int:feature_id>/', views.FeatureRequestDetailView.as_view(), name='feature_request_detail'),
 ]
